445ec79cdcc95c8eb0419284ff42cea2326d7844
Abama cai <caicaiking@gmail.com>
2021-01-23 07:33:54 +0800

    注释了array.hpp

--- src/array.hpp
+++ src/array.hpp
@@ -29,118 +29,134 @@ namespace zmq
     //  Base class for objects stored in the array. Note that each object can
     //  be stored in at most one array.
 
+    /**!
+    * @class array_item_t 要存储在array里面的object的基类，而且每个object只能存储在最多的一个array里。
+    *为啥呢，因为里面的index是一个值，没有相关的数据结构维护这个object和其他array的关系。 
+    */
     class array_item_t
     {
     public:
-
-        inline array_item_t () :
-            array_index (-1)
+        inline array_item_t() : array_index(-1)
         {
         }
 
         //  The destructor doesn't have to be virtual. It is mad virtual
         //  just to keep ICC and code checking tools from complaining.
-        inline virtual ~array_item_t ()
+        inline virtual ~array_item_t()
         {
         }
 
-        inline void set_array_index (int index_)
+        inline void set_array_index(int index_) //记录这个object在array里的位置
         {
             array_index = index_;
         }
 
-        inline int get_array_index ()
+        inline int get_array_index() //获取这个object在array里的位置
         {
             return array_index;
         }
 
     private:
-
         int array_index;
 
-        array_item_t (const array_item_t&);
-        void operator = (const array_item_t&);
+        array_item_t(const array_item_t &);
+        void operator=(const array_item_t &);
     };
 
     //  Fast array implementation with O(1) access to item, insertion and
     //  removal. Array stores pointers rather than objects. The objects have
     //  to be derived from array_item_t class.
-
-    template <typename T> class array_t
+    /**!
+     * 
+     * @class array_t 实现了一个在O(1)时间复杂度内完成查询，插入和移除操作的数组，array
+     * 存储的是指针类型而不是object，这些object可以继承array_item_t。
+     * 
+     * 为什么要继承array_item_t 呢？
+     * 
+     * 因为 在array_t 中要求的T 类型要有两个函数：
+     *      set_array_index (int index_)
+     *      get_array_index 
+     * 
+    */
+    template <typename T>
+    class array_t
     {
     public:
+        typedef typename std::vector<T *>::size_type size_type;
 
-        typedef typename std::vector <T*>::size_type size_type;
-
-        inline array_t ()
+        inline array_t()
         {
         }
 
-        inline ~array_t ()
+        inline ~array_t()
         {
         }
 
-        inline size_type size ()
+        inline size_type size()
         {
-            return items.size ();
+            return items.size();
         }
 
-        inline bool empty ()
+        inline bool empty()
         {
-            return items.empty ();
+            return items.empty();
         }
 
-        inline T *&operator [] (size_type index_)
+        inline T *&operator[](size_type index_) //因为里面存储的是T* 所以返回的是指针的引用
         {
-            return items [index_];
+            return items[index_];
         }
 
-        inline void push_back (T *item_)
+        inline void push_back(T *item_) //设计的精妙就是在这里，将T*的item 存储在array中
         {
             if (item_)
-                item_->set_array_index (items.size ());
-            items.push_back (item_);
+                item_->set_array_index(items.size()); //先将item在array中的位置设置好
+            items.push_back(item_);
         }
 
-        inline void erase (T *item_) {
-            erase (item_->get_array_index ());
-        }
-
-        inline void erase (size_type index_) {
-            if (items.back ())
-                items.back ()->set_array_index (index_);
-            items [index_] = items.back ();
-            items.pop_back ();
-        }
-
-        inline void swap (size_type index1_, size_type index2_)
+        /**!
+         * 删除item就是删除item在array中的位置
+         * */
+        inline void erase(T *item_)
         {
-            if (items [index1_])
-                items [index1_]->set_array_index (index2_);
-            if (items [index2_])
-                items [index2_]->set_array_index (index1_);
-            std::swap (items [index1_], items [index2_]);
+            erase(item_->get_array_index());
         }
 
-        inline void clear ()
+        inline void erase(size_type index_)
         {
-            items.clear ();
+            if (items.back())
+                items.back()->set_array_index(index_); //更新在后元素的index成要删除的index
+            items[index_] = items.back();              //array因为存储的是指针，将当前index的位置复制成末尾的元素
+            items.pop_back();                          //弹出末尾的元素，避免重复
         }
 
-        inline size_type index (T *item_)
+        inline void swap(size_type index1_, size_type index2_)
         {
-            return (size_type) item_->get_array_index ();
+            if (items[index1_])
+                items[index1_]->set_array_index(index2_); //先更新交换后的位置
+            if (items[index2_])
+                items[index2_]->set_array_index(index1_);
+            std::swap(items[index1_], items[index2_]); //交换两个指针
+        }
+
+        inline void clear()
+        {
+            items.clear();
+        }
+
+        inline size_type index(T *item_)
+        {
+            return (size_type)item_->get_array_index();
         }
 
     private:
-
-        typedef std::vector <T*> items_t;
+        typedef std::vector<T *> items_t;
         items_t items;
 
-        array_t (const array_t&);
-        void operator = (const array_t&);
+        array_t(const array_t &);
+        void operator=(const array_t &);
     };
 
-}
+} // namespace zmq
 
 #endif
